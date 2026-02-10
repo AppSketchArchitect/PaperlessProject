5cb75678809f111116f4bf2168e5ad2989e6112a
Enzo Romoli <enzoromoli44@gmail.com>
2026-02-10 12:10:16 +0100

    Add paperless dependencies

---
+++ paperless-install.sh
@@ -0,0 +1,9 @@
+apt install python3 python3-pip python3-dev imagemagick fonts-liberation gnupg libpq-dev default-libmysqlclient-dev pkg-config libmagic-dev libzbar0 poppler-utils
+
+apt install unpaper ghostscript icc-profiles-free qpdf libxml2 pngquant zlib1g tesseract-ocr
+
+apt install build-essential python3-setuptools python3-wheel
+
+apt install redis-server
+
+apt install postgresql
