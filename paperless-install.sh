d3ccc92242a24beea8632c2959e3086c32886384
AppSketchArchitect <enzoromoli44@gmail.com>
2026-02-11 13:00:36 +0100

    stash

--- paperless-install.sh
+++ paperless-install.sh
@@ -17,8 +17,6 @@ sudo tar -xf paperless-ngx-v1.10.2.tar.xz
 
 sudo cp -a ./paperless-ngx/. /opt/paperless
 
-sudo chown paperless:paperless /opt/paperless
-
 sudo mkdir /opt/paperless/media
 sudo mkdir /opt/paperless/data
 sudo mkdir /opt/paperless/consume
@@ -27,4 +25,26 @@ sudo chown paperless:paperless /opt/paperless/media
 sudo chown paperless:paperless /opt/paperless/data
 sudo chown paperless:paperless /opt/paperless/consume
 
-sudo -Hu paperless pip3 install -r requirements.txt
+sudo apt install python3-pip sudo apt install python3-venv
+
+
+cd /opt/paperless
+
+# Install python 3.10.12 to prevent from python3.13 of Ubuntu trixie 13 because of paperless pip dependencies
+sudo apt install -y build-essential libssl-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libffi-dev zlib1g-dev
+sudo wget https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tgz
+sudo tar -xvf Python-3.10.12.tgz
+cd ./Python-3.10.12
+sudo ./configure --enable-optimizations
+sudo make altinstall
+cd ../
+
+./Python-3.10.12/python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+cd ../src
+python3 -m pip install django/
+python3 manage.py migrate
+
+chown paperless:paperless -R venv
+sudo chown paperless:paperless /opt/paperless
