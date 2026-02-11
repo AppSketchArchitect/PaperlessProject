5329e7329b0bb9aa8ca25f3ed5f8692faf67cf1a
AppSketchArchitect <enzoromoli44@gmail.com>
2026-02-11 10:43:23 +0100

    * temp

--- paperless-install.sh
+++ paperless-install.sh
@@ -1,24 +1,30 @@
 
 # Install dependencies
-apt install python3 python3-pip python3-dev imagemagick fonts-liberation gnupg libpq-dev default-libmysqlclient-dev pkg-config libmagic-dev libzbar0 poppler-utils
-apt install unpaper ghostscript icc-profiles-free qpdf libxml2 pngquant zlib1g tesseract-ocr
-apt install build-essential python3-setuptools python3-wheel
-apt install redis-server
-apt install postgresql
+sudo apt install python3 python3-pip python3-dev imagemagick fonts-liberation gnupg libpq-dev default-libmysqlclient-dev pkg-config libmagic-dev libzbar0 poppler-utils
+sudo apt install unpaper ghostscript icc-profiles-free qpdf libxml2 pngquant zlib1g tesseract-ocr
+sudo apt install build-essential python3-setuptools python3-wheel
+sudo apt install redis-server
+sudo apt install postgresql
 
 # 
-adduser paperless --system --home /opt/paperless --group
+sudo adduser paperless --system --home /opt/paperless --group
 
-apt install curl
+sudo apt install curl
 
-curl -O -L https://github.com/paperless-ngx/paperless-ngx/releases/download/v1.10.2/paperless-ngx-v1.10.2.tar.xz
+sudo curl -O -L https://github.com/paperless-ngx/paperless-ngx/releases/download/v1.10.2/paperless-ngx-v1.10.2.tar.xz
 
-tar -xf paperless-ngx-v1.10.2.tar.xz
+sudo tar -xf paperless-ngx-v1.10.2.tar.xz
 
-cp -a ./paperless-ngx/. /opt/paperless
+sudo cp -a ./paperless-ngx/. /opt/paperless
 
-# Install frontend dependencies
-apt-get install nodejs npm
-npm install -g pnpm
-pnpm setup
-pnpm install -g @angular/cli
+sudo chown paperless:paperless /opt/paperless
+
+sudo mkdir /opt/paperless/media
+sudo mkdir /opt/paperless/data
+sudo mkdir /opt/paperless/consume
+
+sudo chown paperless:paperless /opt/paperless/media
+sudo chown paperless:paperless /opt/paperless/data
+sudo chown paperless:paperless /opt/paperless/consume
+
+sudo -Hu paperless pip3 install -r requirements.txt
