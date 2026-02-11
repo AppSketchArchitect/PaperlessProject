0d8731c169fcca74249be3025cbd20d49898ce65
AppSketchArchitect <enzoromoli44@gmail.com>
2026-02-11 09:31:04 +0100

    * update

--- paperless-install.sh
+++ paperless-install.sh
@@ -1,13 +1,12 @@
+
+# Install dependencies
 apt install python3 python3-pip python3-dev imagemagick fonts-liberation gnupg libpq-dev default-libmysqlclient-dev pkg-config libmagic-dev libzbar0 poppler-utils
-
 apt install unpaper ghostscript icc-profiles-free qpdf libxml2 pngquant zlib1g tesseract-ocr
-
 apt install build-essential python3-setuptools python3-wheel
-
 apt install redis-server
-
 apt install postgresql
 
+# 
 adduser paperless --system --home /opt/paperless --group
 
 apt install curl
@@ -17,3 +16,9 @@ curl -O -L https://github.com/paperless-ngx/paperless-ngx/releases/download/v1.1
 tar -xf paperless-ngx-v1.10.2.tar.xz
 
 cp -a ./paperless-ngx/. /opt/paperless
+
+# Install frontend dependencies
+apt-get install nodejs npm
+npm install -g pnpm
+pnpm setup
+pnpm install -g @angular/cli
