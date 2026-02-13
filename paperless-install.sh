ebf85f8fe7af24721c1348208797a159da1f24f8
vboxuser <vboxuser@vbox.home>
2026-02-13 11:52:36 +0100

    Finish Installation Part

--- paperless-install.sh
+++ paperless-install.sh
@@ -1,50 +1,77 @@
+#!/bin/bash
 
-# Install dependencies
-sudo apt install python3 python3-pip python3-dev imagemagick fonts-liberation gnupg libpq-dev default-libmysqlclient-dev pkg-config libmagic-dev libzbar0 poppler-utils
-sudo apt install unpaper ghostscript icc-profiles-free qpdf libxml2 pngquant zlib1g tesseract-ocr
-sudo apt install build-essential python3-setuptools python3-wheel
-sudo apt install redis-server
-sudo apt install postgresql
+if [[ $EUID -ne 0 ]]; then
+   echo "Ce script doit être exécuté en sudo"
+   exit 1
+fi
 
-# 
-sudo adduser paperless --system --home /opt/paperless --group
+# Paperless Installation
+apt update
+apt install -y python3 python3-pip python3-dev imagemagick fonts-liberation gnupg libpq-dev default-libmysqlclient-dev pkg-config libmagic-dev libzbar0 poppler-utils
+apt install -y apt install unpaper ghostscript icc-profiles-free qpdf libxml2 pngquant zlib1g tesseract-ocr
+apt install -y build-essential python3-setuptools python3-wheel
+apt install -y redis-server
+apt install -y postgresql
 
-sudo apt install curl
+adduser paperless --system --home /opt/paperless --group
 
-sudo curl -O -L https://github.com/paperless-ngx/paperless-ngx/releases/download/v1.10.2/paperless-ngx-v1.10.2.tar.xz
+apt install -y curl
+curl -O -L https://github.com/paperless-ngx/paperless-ngx/releases/download/v2.20.6/paperless-ngx-v2.20.6.tar.xz
+tar -xf paperless-ngx-v2.20.6.tar.xz
 
-sudo tar -xf paperless-ngx-v1.10.2.tar.xz
+mkdir /opt/paperless
+cp -a ./paperless-ngx/. /opt/paperless
 
-sudo cp -a ./paperless-ngx/. /opt/paperless
-
-sudo mkdir /opt/paperless/media
-sudo mkdir /opt/paperless/data
-sudo mkdir /opt/paperless/consume
-
-sudo chown paperless:paperless /opt/paperless/media
-sudo chown paperless:paperless /opt/paperless/data
-sudo chown paperless:paperless /opt/paperless/consume
-
-sudo apt install python3-pip sudo apt install python3-venv
-
+mkdir /opt/paperless/media
+mkdir /opt/paperless/data
+mkdir /opt/paperless/consume
 
 cd /opt/paperless
+apt install -y python3.13-venv
+python3 -m venv venv
+sudo -u paperless bash -c "source venv/bin/activate && pip install -r requirements.txt"
+sudo -u paperless bash -c "source venv/bin/activate && cd src && python manage.py migrate"
 
-# Install python 3.10.12 to prevent from python3.13 of Ubuntu trixie 13 because of paperless pip dependencies
-sudo apt install -y build-essential libssl-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libffi-dev zlib1g-dev
-sudo wget https://www.python.org/ftp/python/3.10.12/Python-3.10.12.tgz
-sudo tar -xvf Python-3.10.12.tgz
-cd ./Python-3.10.12
-sudo ./configure --enable-optimizations
-sudo make altinstall
-cd ../
+cp scripts/paperless-*.service /etc/systemd/system
 
-./Python-3.10.12/python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-cd ../src
-python3 -m pip install django/
-python3 manage.py migrate
+chown paperless:paperless /opt/paperless/media
+chown paperless:paperless /opt/paperless/data
+chown paperless:paperless /opt/paperless/consume
 
-chown paperless:paperless -R venv
-sudo chown paperless:paperless /opt/paperless
+# SystemD automatic start
+cat > "/etc/systemd/system/paperless-autostart.service" <<EOF
+[Unit]
+Description=Paperless autostart
+[Service]
+User=paperless
+WorkingDirectory=/opt/paperless/src
+ExecStart=/opt/paperless/venv/bin/python3 manage.py runserver
+Restart=always
+ProtectKernelTunables=yes
+ProtectKernelModules=yes
+ProtectControlGroups=yes
+PrivateDevices=yes
+RestrictSUIDSGID=true
+ProtectSystem=strict
+ReadWritePaths=/opt/paperless/data
+ReadWritePaths=/opt/paperless/media
+ReadWritePaths=/opt/paperless/consume
+CapabilityBoundingSet=CAP_NET_BIND_SERVICE CAP_DAC_READ_SEARCH
+PrivateTmp=yes
+NoNewPrivileges=true
+RestrictNamespaces=uts ipc pid user cgroup
+ProtectHome=yes
+MemoryDenyWriteExecute=yes
+RestrictAddressFamilies=AF_INET AF_INET6
+LockPersonality=yes
+ProtectHostname=yes
+ProtectClock=yes
+ProtectKernelLogs=yes
+RestrictRealtime=yes
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable paperless-autostart
+systemctl start paperless-autostart
